a33e80da9f54f262264daa57701522ae81d7edbc
상빈 <zaza1599@gmail.com>
2023-02-20 19:16:10 +0900

    첫번쨰 커밋 생성

---
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -0,0 +1,12 @@
+package edu.kh.git.practice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요~!");
+		System.out.println("Git 연습용 파일입니다.");
+		
+
+	}
+
+}
